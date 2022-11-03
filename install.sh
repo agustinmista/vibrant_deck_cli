e611e57775cf917e441b69aa699b45aba61e6102
Agustín Mista <mista.agustin@gmail.com>
2022-11-03 20:47:03 +0100

    Make the script download the files automatically

--- install.sh
+++ install.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
-BIN=vibrant_deck_cli
-SERVICE=vibrant_deck_cli.service
+set -e
 
-BIN_DEST_DIR=$HOME/.local/bin
+GITHUB_REPO=agustinmista/vibrant_deck_cli
+
+DEFAULT_SATURATION=4
+
+BIN=vibrant_deck_cli
+SERVICE=$BIN.service
+
+BIN_DIR=$HOME/.local/bin
+CONFIG_DIR=$HOME/.config/$BIN
+
 SYSTEMD_USER_DIR=$HOME/.config/systemd/user
 
-echo "Copying $BIN to $BIN_DEST_DIR"
-mkdir -p $BIN_DEST_DIR
-cp $BIN $BIN_DEST_DIR
-chmod +x $BIN_DEST_DIR/$BIN
+# ----------------------------------------
+
+BIN_URL=$(curl -s 'https://api.github.com/repos/$GITHUB_REPO/contents' | jq -r '.[] | select(.name=="$BIN").download_url')
+SERVICE_URL=$(curl -s 'https://api.github.com/repos/$GITHUB_REPO/contents' | jq -r '.[] | select(.name=="$SERVICE").download_url')
+
+echo "Downloading $BIN to $BIN_DIR/$BIN"
+mkdir -p $BIN_DIR
+curl -L $BIN_URL --output $BIN_DIR/$BIN
+chmod +x $BIN_DIR/$BIN
+
+echo "Creating config file at to $CONFIG_DIR/config"
+mkdir -p $CONFIG_DIR
+echo "SATURATION=$DEFAULT_SATURATION" > $CONFIG_DIR/config
 
 echo "Copying $SERVICE to $SYSTEMD_USER_DIR"
-cp $SERVICE $SYSTEMD_USER_DIR
-
-echo "Copying $TIMER to $SYSTEMD_USER_DIR"
-cp $TIMER $SYSTEMD_USER_DIR
+curl -L $SERVICE_URL --output $SYSTEMD_USER_DIR/$SERVICE
 
 echo "Enabling $SERVICE on systemd"
 systemctl --user enable $SERVICE
 
+echo "Done! Try restarting your Steam Deck now :D"
