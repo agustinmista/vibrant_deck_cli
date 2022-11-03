dbc516c2bf97ce49967ef49cea7c2d82e908b5a7
Agustín Mista <mista.agustin@gmail.com>
2022-11-03 21:07:03 +0100

    Fixes in installer script

--- install.sh
+++ install.sh
@@ -3,35 +3,39 @@
 set -e
 
 GITHUB_REPO=agustinmista/vibrant_deck_cli
-
-DEFAULT_SATURATION=4
+API_URL=https://api.github.com/repos/$GITHUB_REPO/contents
 
 BIN=vibrant_deck_cli
-SERVICE=$BIN.service
-
 BIN_DIR=$HOME/.local/bin
-CONFIG_DIR=$HOME/.config/$BIN
 
+SERVICE=$BIN.service
 SYSTEMD_USER_DIR=$HOME/.config/systemd/user
+
+CONFIG_DIR=$HOME/.config/$BIN
+DEFAULT_SATURATION=4
 
 # ----------------------------------------
 
-BIN_URL=$(curl -s 'https://api.github.com/repos/$GITHUB_REPO/contents' | jq -r '.[] | select(.name=="$BIN").download_url')
-SERVICE_URL=$(curl -s 'https://api.github.com/repos/$GITHUB_REPO/contents' | jq -r '.[] | select(.name=="$SERVICE").download_url')
+echo "Looking up the files we need to download ..."
+REPO_CONTENTS=$(curl -s $API_URL)
 
-echo "Downloading $BIN to $BIN_DIR/$BIN"
+BIN_URL=$(echo $REPO_CONTENTS | jq -r --arg file $BIN '.[] | select(.name==$file).download_url')
+SERVICE_URL=$(echo $REPO_CONTENTS | jq -r --arg file $SERVICE '.[] | select(.name==$file).download_url')
+
+echo "Downloading $BIN_URL into $BIN_DIR/$BIN ..."
 mkdir -p $BIN_DIR
-curl -L $BIN_URL --output $BIN_DIR/$BIN
+curl -sL $BIN_URL --output $BIN_DIR/$BIN
 chmod +x $BIN_DIR/$BIN
 
-echo "Creating config file at to $CONFIG_DIR/config"
+echo "Creating config file at to $CONFIG_DIR/config ..."
 mkdir -p $CONFIG_DIR
 echo "SATURATION=$DEFAULT_SATURATION" > $CONFIG_DIR/config
 
-echo "Copying $SERVICE to $SYSTEMD_USER_DIR"
-curl -L $SERVICE_URL --output $SYSTEMD_USER_DIR/$SERVICE
+echo "Downloading $SERVICE_URL into $SYSTEMD_USER_DIR ..."
+curl -sL $SERVICE_URL --output $SYSTEMD_USER_DIR/$SERVICE
 
-echo "Enabling $SERVICE on systemd"
+echo "Enabling $SERVICE on systemd ..."
 systemctl --user enable $SERVICE
 
 echo "Done! Try restarting your Steam Deck now :D"
+echo "The default screen saturation can be changed in $CONFIG_DIR/config"
